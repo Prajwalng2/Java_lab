6479f186b11cf796a0ad13df0aaeb917ccb86011
Prajwal N <118053725+Prajwalng2@users.noreply.github.com>
2023-12-25 19:40:45 +0530

    Add files via upload

---
+++ p8.java
@@ -0,0 +1,31 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class DivisionExample {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter the numerator (positive integer): ");
+            int numerator = scanner.nextInt();
+
+            if (numerator <= 0) {
+                throw new InputMismatchException("Numerator must be a positive integer.");
+            }
+
+            System.out.print("Enter the denominator (non-zero integer): ");
+            int denominator = scanner.nextInt();
+
+            if (denominator == 0 || denominator <= 0) {
+                throw new ArithmeticException("Denominator cannot be zero or negative Number");
+            }
+
+            double result = (double) numerator / denominator;
+            System.out.println("Result of division: " + result);
+        } catch (InputMismatchException | ArithmeticException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+
+        scanner.close();
+    }
+}
