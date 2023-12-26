e12b43572d02de5498a9647fafdd5656aacfe80e
Prajwal N <118053725+Prajwalng2@users.noreply.github.com>
2023-12-26 10:29:31 +0530

    Update p2.java

--- p2.java
+++ p2.java
@@ -1,70 +1,104 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
-
-class Employee {
-    String name, department, designation;
-    int employeeId, age;
-
-    public Employee(String name, int employeeId, String department, int age, String designation) {
-        this.name = name;
-        this.employeeId = employeeId;
-        this.department = department;
-        this.age = age;
-        this.designation = designation;
-    }
-}
 
 public class EmployeeDatabase {
 
-    static List<Employee> employeeList = new ArrayList<>();
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        readEmployeeDetails(5);
-        displayEmployeeDetails();
-        System.out.println("Sum of salaries in Sales department: " + calculateSumSalary("sales"));
+        int numEmployees = 5;
+        Employee[] employees = new Employee[numEmployees];
 
-        Employee highestPaidManager = getHighestPaidManager("purchase");
-        System.out.println(highestPaidManager != null
-                ? "Details of the highest paid manager in Purchase department:\n" + highestPaidManagerDetails(highestPaidManager)
-                : "No manager found in the Purchase department.");
-    }
+        // Read and display details of employees
+        for (int i = 0; i < numEmployees; i++) {
+            System.out.println("Enter details for Employee " + (i + 1) + ":");
+            employees[i] = readEmployeeDetails();
+        }
 
-    static void readEmployeeDetails(int numEmployees) {
-        for (int i = 1; i <= numEmployees; i++) {
-            System.out.println("Enter details for Employee " + i + ":");
-            employeeList.add(new Employee(scanner.next(), scanner.nextInt(), scanner.next(), scanner.nextInt(), scanner.next()));
+        // Display all employees
+        System.out.println("\nDetails of all Employees:");
+        displayAllEmployees(employees);
+
+        // Calculate and display the sum of salary of "sales" department
+        String departmentToSum = "sales";
+        double salesDeptSalarySum = calculateDepartmentSalarySum(employees, departmentToSum);
+        System.out.println("\nSum of salaries for employees in the \"" + departmentToSum + "\" department: " + salesDeptSalarySum);
+
+        // Retrieve details of the highest paid manager in the "purchase" department
+        String departmentToRetrieve = "purchase";
+        Employee highestPaidManager = retrieveHighestPaidManager(employees, departmentToRetrieve);
+
+        if (highestPaidManager != null) {
+            System.out.println("\nDetails of the highest paid manager in the \"" + departmentToRetrieve + "\" department:");
+            displayEmployeeDetails(highestPaidManager);
+        } else {
+            System.out.println("\nNo manager found in the \"" + departmentToRetrieve + "\" department.");
         }
     }
 
-    static void displayEmployeeDetails() {
-        System.out.println("\nEmployee Details:");
-        employeeList.forEach(EmployeeDatabase::displayEmployeeDetails);
-        System.out.println();
+    static Employee readEmployeeDetails() {
+        Employee employee = new Employee();
+        System.out.print("Name: ");
+        employee.name = scanner.nextLine();
+        System.out.print("Emp_id: ");
+        employee.empId = scanner.nextInt();
+        scanner.nextLine(); // Consume the newline character
+        System.out.print("Department: ");
+        employee.department = scanner.nextLine();
+        System.out.print("Age: ");
+        employee.age = scanner.nextInt();
+        scanner.nextLine(); // Consume the newline character
+        System.out.print("Designation: ");
+        employee.designation = scanner.nextLine();
+        System.out.print("Salary: ");
+        employee.salary = scanner.nextDouble();
+        scanner.nextLine(); // Consume the newline character
+        return employee;
     }
 
-    static String highestPaidManagerDetails(Employee employee) {
-        return "Name: " + employee.name + "\nEmployee ID: " + employee.employeeId +
-                "\nDepartment: " + employee.department + "\nAge: " + employee.age +
-                "\nDesignation: " + employee.designation + "\n--------------------------";
+    static void displayAllEmployees(Employee[] employees) {
+        for (Employee employee : employees) {
+            displayEmployeeDetails(employee);
+        }
     }
 
     static void displayEmployeeDetails(Employee employee) {
-        System.out.println(highestPaidManagerDetails(employee));
+        System.out.println("Name: " + employee.name);
+        System.out.println("Emp_id: " + employee.empId);
+        System.out.println("Department: " + employee.department);
+        System.out.println("Age: " + employee.age);
+        System.out.println("Designation: " + employee.designation);
+        System.out.println("Salary: " + employee.salary);
+        System.out.println();
     }
 
-    static double calculateSumSalary(String department) {
-        return employeeList.stream()
-                .filter(e -> e.department.equalsIgnoreCase(department))
-                .mapToDouble(e -> 50000) // Placeholder for salary calculation logic
-                .sum();
+    static double calculateDepartmentSalarySum(Employee[] employees, String department) {
+        double sum = 0;
+        for (Employee employee : employees) {
+            if (employee.department.equalsIgnoreCase(department)) {
+                sum += employee.salary;
+            }
+        }
+        return sum;
     }
 
-    static Employee getHighestPaidManager(String department) {
-        return employeeList.stream()
-                .filter(e -> e.department.equalsIgnoreCase(department) && e.designation.equalsIgnoreCase("manager"))
-                .max(java.util.Comparator.comparing(e -> 80000)) // Placeholder for salary calculation logic
-                .orElse(null);
+    static Employee retrieveHighestPaidManager(Employee[] employees, String department) {
+        Employee highestPaidManager = null;
+        double maxSalary = 0;
+        for (Employee employee : employees) {
+            if (employee.department.equalsIgnoreCase(department) && employee.designation.equalsIgnoreCase("manager") && employee.salary > maxSalary) {
+                maxSalary = employee.salary;
+                highestPaidManager = employee;
+            }
+        }
+        return highestPaidManager;
+    }
+
+    static class Employee {
+        String name;
+        int empId;
+        String department;
+        int age;
+        String designation;
+        double salary;
     }
 }
